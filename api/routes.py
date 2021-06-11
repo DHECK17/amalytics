e9596b28a55a24ba28eb236fb8fb43ee43aacd16
Amal Shaji <amalshajid@gmail.com>
2021-06-11 15:58:18 +0530

    fixed datetime error

--- api/routes.py
+++ api/routes.py
@@ -49,7 +49,7 @@ def get_location_and_create_click(ip: str, data: dict):
     extraas = {
         "ip": ip,
         "location": get_country_from_ip(ip),
-        "created_at": datetime.now(),
+        "created_at": datetime.now().isoformat(),
     }
     if data.get("outerWidth") >= 1000:
         data["device"] = "Desktop"
